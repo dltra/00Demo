6e8f6e28bf64e6ec11cea985808dbb4c607ee7c4
dltra <dltra@fcps.edu>
2019-09-07 17:56:31 -0400

    Read text file into arraylist

--- app/src/main/java/com/traf1/demo/MainActivity.java
+++ app/src/main/java/com/traf1/demo/MainActivity.java
@@ -9,8 +9,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     Button submitButton;
@@ -38,18 +41,18 @@ public class MainActivity extends AppCompatActivity {
     }
     public void submit(View view) {//process button onClick event
         String text = "";
+        ArrayList<String> texts = new ArrayList<>();
         try{
             InputStream inputStream = getAssets().open("myTextFile.txt");
             /** For use with raw file
              *  InputStream inputStream = getResources().openRawResource(R.raw.my_text_file);
              */
-            int size = inputStream.available();
-            byte[] buffer = new byte[size];
-            inputStream.read(buffer);
-            inputStream.close();
-            text = new String(buffer);
+            BufferedReader bufferReader = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
+            while((text = bufferReader.readLine())!=null) {
+                texts.add(text);
+            }
         }catch (IOException e){}
-        displayText.setText(text);
+        displayText.setText(texts.get(0));
         Toast.makeText(getApplicationContext(),"Done reading.",Toast.LENGTH_SHORT).show();
     }
 }
